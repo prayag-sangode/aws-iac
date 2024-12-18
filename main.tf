65f2a68a56b5b824d74b205d6ddd9ec9938430c9
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2024-12-18 19:36:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "tfstate19159-tf"
+    bucket         = "tfstate19159-bkt"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
